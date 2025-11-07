cdcfc47285b028e110d432d90f48800d778a7c54
Aaron Harper <aaron@inngest.com>
2025-11-07 13:26:42 -0500

    Fix inngestify not accepting array of workflows

--- packages/inngest/src/inngestify.ts
+++ packages/inngest/src/inngestify.ts
@@ -6,51 +6,52 @@ import { type InngestClient, type StepExt } from "./client";
 
 export function inngestify(
   client: InngestClient,
-  workflow: Workflow<any, any, ExtDefault, ExtDefault, StepExt>
+  workflows: Workflow<any, any, ExtDefault, ExtDefault, StepExt>[]
 ): ServeHandlerOptions {
   const inngest = new Inngest({ id: client.id });
-  const functions = inngest.createFunction(
-    {
-      id: workflow.id,
-    },
-    { event: "workflow/say-hi" },
-    async (ctx) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return workflow.handler(
-        {
-          runId: ctx.runId,
-          input: {
-            ...ctx.event,
-            id: ctx.event.id ?? crypto.randomUUID(),
-            ext: {},
-            time: new Date(),
-            type: "invoke",
+  const functions = workflows.map((workflow) => {
+    return inngest.createFunction(
+      {
+        id: workflow.id,
+      },
+      { event: "workflow/say-hi" },
+      async (ctx) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return workflow.handler(
+          {
+            runId: ctx.runId,
+            input: {
+              ...ctx.event,
+              id: ctx.event.id ?? crypto.randomUUID(),
+              ext: {},
+              time: new Date(),
+              type: "invoke",
 
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            data: ctx.event.data,
-          },
-          ext: {},
-        },
-        {
-          ext: {
-            sleepUntil: async (stepId: string, wakeupAt: Date) => {
-              await ctx.step.sleep(stepId, wakeupAt.getTime() - Date.now());
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              data: ctx.event.data,
             },
+            ext: {},
           },
-          run: <T>(stepId: string, handler: () => T) => {
-            //
-            // TODO: type return properly
-            return ctx.step.run(stepId, handler) as Promise<T>;
-          },
-          sleep: (stepId: string, duration: number) => {
-            return ctx.step.sleep(stepId, duration);
-          },
-        }
-      );
-    }
-  );
+          {
+            ext: {
+              sleepUntil: async (stepId: string, wakeupAt: Date) => {
+                await ctx.step.sleep(stepId, wakeupAt.getTime() - Date.now());
+              },
+            },
+            run: <T>(stepId: string, handler: () => T) => {
+              return ctx.step.run(stepId, handler) as Promise<T>;
+            },
+            sleep: (stepId: string, duration: number) => {
+              return ctx.step.sleep(stepId, duration);
+            },
+          }
+        );
+      }
+    );
+  });
+
   return {
     client: inngest,
-    functions: [functions],
+    functions,
   };
 }
