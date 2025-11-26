3a5fc635c838fcb53d5f6084a18bfe4c052c7fcd
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-11-26 13:00:00 -0500

    Add snippet

--- docs/src/routes/__root.tsx
+++ docs/src/routes/__root.tsx
@@ -48,6 +48,17 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             {children}
           </RootProvider>
         <Scripts />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
       </body>
     </html>
   );
