17652968e0ff30486b6ad458ab3cc35dc976e205
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-11-26 13:03:11 -0500

    Update for client

--- docs/src/routes/__root.tsx
+++ docs/src/routes/__root.tsx
@@ -48,14 +48,14 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             {children}
           </RootProvider>
         <Scripts />
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.VITE_APP_GA_MEASUREMENT_ID}`}></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+              gtag('config', '${process.env.VITE_APP_GA_MEASUREMENT_ID}');
             `,
           }}
         />
